847e2c621db8b2aa9428975a62dec40d7ac87623
Nikhil Bhadait <codesharad@gmail.com>
2024-10-26 08:44:10 +0530

    updated ui flow

--- src/components/ui/certificate-download.tsx
+++ src/components/ui/certificate-download.tsx
@@ -39,7 +39,6 @@ const checkCompletion = async (
     (res) => res.json()
   );
   setResData(res);
-  console.log(res);
   return res.message == "Email not found" ? false : true;
 };
 
@@ -57,6 +56,7 @@ export default function CertificateDownload() {
   const [error, setError] = useState("");
   const [resData, setResData] = useState<CertificateResponse | null>(null);
   const [celebrate, setCelebrate] = useState(false);
+  const [isDownloading, setIsDownloading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -74,15 +74,12 @@ export default function CertificateDownload() {
           "Please enter the email address you used when filling out the Google form."
         );
       }
-      toast.success(
-        " Certificate generated successfully! ",
-        {
-          duration: 5000,
-          position: "top-right",
-          closeButton: true,
-          icon: "🎉",
-        }
-      );
+      toast.success(" Certificate generated successfully! ", {
+        duration: 5000,
+        position: "top-right",
+        closeButton: true,
+        icon: "🎉",
+      });
     } catch (error) {
       console.log(error);
       setError("An error occurred. Please try again.");
@@ -95,13 +92,12 @@ export default function CertificateDownload() {
   return (
     <div className="">
       <div>
-        {celebrate && <Realistic autorun={{ duration: 3, speed: 15 }} />}
+        {celebrate && <Realistic autorun={{ duration: 4, speed: 30 }} />}
       </div>
       <Card className="lg:max-w-[900px] lg:p-14 lg:max-h-[500px] lg:grid lg:grid-cols-3 place-content-center ">
         <CardHeader className="col-span-1 w-full">
           <div className="relative mx-auto">
             <img
-              fetchPriority="low"
               src={logo}
               className="h-44 w-44 bg-blend-lighten blur-lg animate-pulse  relative"
             />
@@ -171,20 +167,29 @@ export default function CertificateDownload() {
               <Button className="py-6 poppins-regular lg:mt-9">
                 <a
                   download={true}
-                  href={resData?.certificate_url}
-                  onClick={() => {
-                    setCelebrate(true);
+                  onLoad={() => console.log("downloaded the pdf")}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = resData?.certificate_url || "";
+                    setIsDownloading(true);
                     toast.success("Congratulations!", {
                       duration: 5000,
                       position: "top-right",
                       closeButton: true,
                       icon: "🎉",
-                    }); 
-                    
+                    });
+                    setCelebrate(true);
+                    setTimeout(() => {
+                      setIsDownloading(false);
+                    }, 2500);
                   }}
                   className="w-full flex gap-1 items-center justify-center text-xl "
                 >
-                  🎉 Download Certificate 🎉
+                  {isDownloading ? (
+                    <span>📥 Downloading...</span>
+                  ) : (
+                    <span>🎉 Download Certificate 🎉</span>
+                  )}
                 </a>
               </Button>
             </div>
