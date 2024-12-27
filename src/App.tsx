e9d67ef22365b18485c52844b57fddeb4e4e7dd2
Nikhil Bhadait <codesharad@gmail.com>
2024-12-27 19:24:52 +0530

    updated ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import "./App.css";
 import CertificateDownload from "./components/ui/certificate-download";
-import FlickeringGrid from "./components/ui/flickering-grid";
 import { Toaster } from "./components/ui/sonner";
 
 export function App() {
   return (
-    <main className="min-h-screen bg-gradient-to-r   from-blue-100 to-purple-100 flex items-center justify-center  p-4 overflow-hidden sm:p-0">
-      <CertificateDownload  />
-      {/* <div className="relative h-[500px] rounded-lg  w-full bg-transparent  overflow-hidden border"> */}
-        <FlickeringGrid
-          className="z-0 absolute inset-0 size-full overflow-hidden"
-          squareSize={16}
-          gridGap={6}
-          color="#6B7280"
-          maxOpacity={0.5}
-          flickerChance={0.1}
-          height={1000}
-          width={2000}
-        />
-      {/* </div> */}
+    <main className="min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 flex items-center justify-center  p-4 overflow-auto sm:p-0">
+      <p className="z-10 whitespace-pre-wrap ">
+        <CertificateDownload />
+      </p>
       <Toaster />
     </main>
   );
